271910aebdfba435e62f708f3fc82b717f2d6331
Siddharth Shrimali <r.siddharth.shrimali@gmail.com>
2026-05-25 12:00:38 +0530

    create blob-candiate suitable for pruning

--- builtin/drop-blobs.c
+++ builtin/drop-blobs.c
@@ -9,3 +9,20 @@
 #include "revision.h"
 #include "promisor-remote.h"
 #include "path-walk.h"
+
+/*
+ * storing candidate blob that survived all three filters:
+ *   1 - type = OBJ_BLOB
+ *   2 - inflated size >= min_size
+ *   3 - not referenced by the current index (default) or
+ *	 not an already-promised object
+ *
+ * store the inflated size so that we can report total reclaimable
+ * space without a second pass.
+ */
+
+struct blob_candidate {
+	struct object_id oid;
+	unsigned long size; /* storing the size of the blob to
+				avoid parsing it later (inflated size) */
+};
